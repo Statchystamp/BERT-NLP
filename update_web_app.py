b6fee03c413b6175b834be8da319cd0e35e973a4
Shunthosh Luxmykanth <shunthoshl@stax.com>
2023-03-28 14:34:30 +0530

    initial commit

---
+++ update_web_app.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+import pandas as pd
+
+st.write("""
+
+# BERT NLP Platform
+
+Enter the keyword below
+
+""")
+
+input_1 = st.text_input("Enter the keyword here")
+
+# Function comes here 
+
+# st.dataframe()
